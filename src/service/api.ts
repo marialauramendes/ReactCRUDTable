afdc360b411b0fbe0028b4c58da944e7812bedc4
Maria Laura Mendes <marialaurasmc@icloud.com>
2023-08-21 22:18:36 -0300

    Adiciona metodos http post, put e delete

--- src/service/api.ts
+++ src/service/api.ts
@@ -1,3 +1,10 @@
-import { get } from './axios-http-request';
+import { PostType } from '../types';
+import { get, post, put, del } from './axios-http-request';
 
 export const getAllPosts = async () => await get('posts');
+
+export const createNewPost = async (data: PostType) => await post('posts', data);
+
+export const editPost = async (postId: number, data: PostType) => await put(`posts/${postId}`, data);
+
+export const deletePost = async (postId: number,) => await del(`posts/${postId}`);
